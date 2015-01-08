eccdbf7f038ebfc16011287ebb424fcc8828b0e9
Manish Johari <manish.johari@kiwitech.com>
2015-01-08 15:51:12 +0530

    Api for login and signup

--- api_data.js
+++ api_data.js
@@ -32,7 +32,7 @@ define({ api: [
       "examples": [
         {
           "title": "Success (201):",
-          "content": "{\n\t\"user_id\":  2,\n\t\"auth_token\": \"xyzabc\" \n}"
+          "content": "{\n\t\"user_id\":1,\"country_code\":91,\"phone_no\":\"9971480801\",\"verification_token\":\"778fb\" \n}"
         }
       ]
     },
@@ -41,6 +41,45 @@ define({ api: [
         {
           "title": "Error (422):",
           "content": "{\n\t \"errors\" : \n\t {\n\t\t \"phone_no\": \"Phone_no has already been taken.\" \n\t}\n}"
+        }
+      ]
+    },
+  },
+
+  {
+    "type": "get",
+    "url": "/users/confirmation",
+    "title": "Confirm token",
+    "name": "Confirm_Token",
+    "description": "API will verify the confirmation token.",
+    "group": "Login",
+    "version": "1",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "field": "user[confirmation_token]",
+            "optional": false,
+            "description": "Provide the confirmation_token sent to user via sms."
+          },
+        ]
+      }
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response (200):",
+          "content": "{\n\t \"user_id\":1,\"country_code\":91,\"phone_no\":\"9971480801\",\"token\":\"nBbemTsS4U4ia7YAsKbW\"\n}\n"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response (422):",
+          "content": "{\n\t \"errors\": \n\t\t {\n\t\t\t\"confirmation_token\": \"Confirmation token is invalid.\", \n\t\t\t\"generic_errors\": \"Confirmation token is invalid.\"\n\t\t}\n}\n"
         }
       ]
     },
--- api_project.js
+++ api_project.js
@@ -1,5 +1,5 @@
 define({
-  "name": "APIDOC",
+  "name": "SeeYouAll : APIDOC",
   "version": "1",
   "description": "Documentation for RESTful web APIs<br /><div class='pull-left'><h4>Base URL- http://dev.seeyouall.com/api/v1 </h4></div>\n <h5>Headers:</h5> Accept: application/json \n Content-Type: application/json \n<h5>Success</h5><li>200 :OK</li><li>201 :Created</li> \n<h5>Error Code:</h5> \n <ul><li>400 :Application error</li><li>401 :Authentication failed</li><li>404 :Record Not Found</li><li>409 :Conflict</li><li>422 :Unprocessable Entity</li><li>500 :Server Error</li></ul>",
   "apidoc": "",
